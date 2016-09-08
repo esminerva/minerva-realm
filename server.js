c3fefc8ee90aff4f9ab3cda1fcbb6d5ef65369d6
Joy Chang <joychang@us.ibm.com>
2016-09-08 14:21:44 -0500

    Placeholder page

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3456));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
