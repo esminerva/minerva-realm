f8477bfb1cae413b57f44b1348816e3db1ebb86d
Yi-An Chang <joy01804@gmail.com>
2018-04-29 07:47:39 -0500

    poc for encode decode

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 
@@ -7,10 +9,29 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get('/', (req, res) => {
+  res.render('');
+});
+
+app.get('/profiles', (req, res) => {
+  res.render('profiles');
+});
+
+var base64data;
+app.get('/scrambled', (req, res) => {
+  let fs = require('fs-extra');
+  let data = fs.readFileSync('./public/9yin-en.html');
+  let buffer = new Buffer(data);
+  base64data = buffer.toString('base64');
+  res.send(base64data);
+});
+
+app.get('/descrambled', (req, res) => {
+  let buffer = new Buffer(base64data || '', 'base64')
+  let decode = buffer.toString('utf-8')
+  res.send(decode);
 });
 
 app.listen(app.get('port'), function() {
